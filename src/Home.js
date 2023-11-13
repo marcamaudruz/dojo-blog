fe1dd8ee6ae1055ce3e690e85f40c6f0644db8af
marcamaudruz <149760434+marcamaudruz@users.noreply.github.com>
2023-11-13 15:52:31 +0000

    fin du tuto :)

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-    const { data: blogs, isPending, error } = useFetch('https://my-json-server.typicode.com/marcamaudruz/dojo-blog/blogs');
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
     return (
     <div className="home">
       { error && <div>{ error }</div> }
